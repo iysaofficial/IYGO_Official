88bc6ff25fb44f804e2a41a5d7769548e469c118
iysaofficial <youngscientist.iysa@gmail.com>
2025-09-03 10:35:22 +0700

    regis & newsletter

--- src/components/home/NewsletterComp.tsx
+++ src/components/home/NewsletterComp.tsx
@@ -1,4 +1,38 @@
+"use client";
+import React, { useEffect } from "react";
+
 const NewsletterComp = () =>{
+    useEffect(() => {
+        const scriptURL =
+        "https://script.google.com/macros/s/AKfycbwfw1dAhzyY6p6crVCCuoEXzb_E7hBOi8cYuIBu_-JZzz2a25h-tCvT6Xd3dUH9Mx2fQA/exec";
+
+        const form = document.forms.namedItem("regist-form1");
+
+        if (form) {
+        const handleSubmit = async (e: Event) => {
+            e.preventDefault();
+            try {
+            await fetch(scriptURL, {
+                method: "POST",
+                body: new FormData(form),
+            });
+            alert("Data sent successfully!");
+            form.reset();
+            } catch (error) {
+            console.error("Error:", error);
+            alert("Failed to send data.");
+            }
+        };
+
+        form.addEventListener("submit", handleSubmit);
+
+        // cleanup listener
+        return () => {
+            form.removeEventListener("submit", handleSubmit);
+        };
+        }
+    }, []);
+
     return(
         <>
         <section className="newsletter-section">
@@ -6,9 +40,9 @@ const NewsletterComp = () =>{
                 <div className="newsletter-subscribe_wrapper">
                     <h2>Subscribe Now!</h2>
                     <p>Subscribe to our newsletter to receive our latest news</p>
-                    <form action="https://formspree.io/f/xoqgjrok" method="POST">
+                    <form action="" method="POST" name="regist-form1">
                         <div className="inner">
-                            <input type="email" name="email" placeholder="Enter Email" />
+                            <input type="email" name="Email" placeholder="Enter Email" />
                         </div>
                         <button type="submit">Subscribe</button>
                     </form>
